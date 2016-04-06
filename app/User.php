450a7625278d60d996746957ff50c387bef91822
vagrant <vagrant@laravel.app>
2016-04-05 20:33:08 -0400

    Auth Changes

--- app/User.php
+++ app/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'first_name', 'last_name',  'email', 'password', 'phone',
+        'name', 'first_name', 'last_name', 'phone', 'email', 'password', 'phone',
     ];
 
     /**
