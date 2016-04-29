5799767b615370e8d30d2036a259c0028ebf771e
vagrant <vagrant@localhost>
2016-04-29 00:28:55 -0400

    Touched up Erik's JS file.

--- public/js/characterCreation.js
+++ public/js/characterCreation.js
@@ -1,18 +1,13 @@
-
-    
 $(function () {
   $('[data-toggle="popover"]').popover()
 });
-
-
-
 
     
 function chooseClass(name){
     var classDescription = name;
     document.getElementById("classDescription").innerHTML = classDescription;
     document.getElementById("classDescription").style.display = "block";
-        };
+}
 
 
 function chooseType(type){
@@ -27,9 +22,7 @@ function chooseType(type){
     document.getElementById("classDescription").style.display = "none";
     document.getElementById(element).style.display = "block";
     console.log(element);
-};
-
-
+}
 
 
 function classicDice(){
@@ -63,60 +56,62 @@ function classicDice(){
     var cha3 = Math.floor((Math.random() * 6) +1 );
     var cha = (cha1 + cha2 +cha3);
     document.getElementById("charisma").innerHTML = (cha1 + " + " + cha2 + " + " + cha3 + " = " + cha);
-    };
-    
+}
+
+
 function removeLowest(){
     var str = [];
     for (i=1; i<=4; i++){
-    str.push(Math.floor((Math.random() * 6) +1 ));
-    };
+        str.push(Math.floor((Math.random() * 6) +1 ));
+    }
     str.sort(function(a, b){return a-b});
     strTotal = (str[1] + str[2] + str[3]);
     document.getElementById("strength").innerHTML = ("(" + str[0] + ")" + " - " + str[1] + " + " + str[2] + " + " + str[3] + " = " + strTotal);
     var dex = [];
     for (i=1; i<=4; i++){
-    dex.push(Math.floor((Math.random() * 6) +1 ));
-    };
+        dex.push(Math.floor((Math.random() * 6) +1 ));
+    }
     dex.sort(function(a, b){return a-b});
     dexTotal = (dex[1] + dex[2] + dex[3]);
     document.getElementById("dexterity").innerHTML = ("(" + dex[0] + ")" + " - " + dex[1] + " + " + dex[2] + " + " + dex[3] + " = " + dexTotal);
     var con = [];
     for (i=1; i<=4; i++){
-    con.push(Math.floor((Math.random() * 6) +1 ));
-    };
+        con.push(Math.floor((Math.random() * 6) +1 ));
+    }
     con.sort(function(a, b){return a-b});
     conTotal = (con[1] + con[2] + con[3]);
     document.getElementById("constitution").innerHTML = ("(" + con[0] + ")" + " - " + con[1] + " + " + con[2] + " + " + con[3] + " = " + conTotal);
     var int = [];
     for (i=1; i<=4; i++){
-    int.push(Math.floor((Math.random() * 6) +1 ));
-    };
+        int.push(Math.floor((Math.random() * 6) +1 ));
+    }
     int.sort(function(a, b){return a-b});
     intTotal = (int[1] + int[2] + int[3]);
     document.getElementById("intelligence").innerHTML = ("(" + int[0] + ")" + " - " + int[1] + " + " + int[2] + " + " + int[3] + " = " + intTotal);
     var wis = [];
     for (i=1; i<=4; i++){
-    wis.push(Math.floor((Math.random() * 6) +1 ));
-    };
+        wis.push(Math.floor((Math.random() * 6) +1 ));
+    }
     wis.sort(function(a, b){return a-b});
     wisTotal = (wis[1] + wis[2] + wis[3]);
     document.getElementById("wisdom").innerHTML = ("(" + wis[0] + ")" + " - " + wis[1] + " + " + wis[2] + " + " + wis[3] + " = " + wisTotal);
     var cha = [];
     for (i=1; i<=4; i++){
-    cha.push(Math.floor((Math.random() * 6) +1 ));
-    };
+        cha.push(Math.floor((Math.random() * 6) +1 ));
+    }
     cha.sort(function(a, b){return a-b});
     chaTotal = (cha[1] + cha[2] + cha[3]);
     document.getElementById("charisma").innerHTML = ("(" + cha[0] + ")" + " - " + cha[1] + " + " + cha[2] + " + " + cha[3] + " = " + chaTotal);
     modifiers();
- };
-    
+}
  
-    function modifiers(){
-        str = document.getElementById('strength');
-        if (str >=12){
-            strMod = Math.floor(str/2 - 5);
-            console.log(strMod);        
-    };
-    elseif (str<)
-};
+ 
+function modifiers(){
+    str = document.getElementById('strength');
+    if (str >=12){
+        strMod = Math.floor(str/2 - 5);
+        console.log(strMod);        
+    }elseif (str<1){
+        
+    }
+}
